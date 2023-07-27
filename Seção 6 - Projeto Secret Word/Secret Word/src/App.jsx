dd235ed625e30a7248abe946685f39a6447b3fe7
Eros Netto <erosnetto1002@gmail.com>
2023-07-26 22:40:23 -0300

    Metade

--- Seção 6 - Projeto Secret Word/Secret Word/src/App.jsx
+++ Seção 6 - Projeto Secret Word/Secret Word/src/App.jsx
@@ -22,8 +22,36 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return {word, category};
+  };
+
   // starts the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    // create an array of letters
+    let wordLetters = word.split('');
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters); //letters
+
     setGameStage(stages[1].name);
   };
 
