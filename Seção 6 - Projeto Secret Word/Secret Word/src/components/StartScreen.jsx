dbae8aacf638e31e80b7fda3c39e08a6bb84496e
Eros Netto <erosnetto1002@gmail.com>
2023-07-28 21:11:22 -0300

    Secret Word

--- Seção 6 - Projeto Secret Word/Secret Word/src/components/StartScreen.jsx
+++ Seção 6 - Projeto Secret Word/Secret Word/src/components/StartScreen.jsx
@@ -3,9 +3,12 @@ import './StartScreen.css';
 const StartScreen = ({ startGame }) => {
   return (
     <div className='start'>
-      <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para começar a jogar</p>
-      <button onClick={startGame}>Começar o jogo</button>
+      <div className="logo">
+        <h1>Secret</h1>
+        <h1>Wo<span>r</span>d</h1>
+      </div>
+
+      <button onClick={startGame}>Play</button>
     </div>
   );
 };
