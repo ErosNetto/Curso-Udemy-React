36b09057b6e761459d98364d971c0cfe41070c1d
Eros Netto <erosnetto1002@gmail.com>
2023-08-02 11:54:30 -0300

    Começo do desafio

--- Seção 7 - Requisições HTTP com React/httpreact/src/App.jsx
+++ Seção 7 - Requisições HTTP com React/httpreact/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - Custom hook
-  const { data: items, httpConfig, loading } = useFetch(url);
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
@@ -64,7 +64,8 @@ function App() {
         <h1>Lista de Produtos</h1>
         {/* 6 - Loading */}
         {loading && <p>Carregando dados...</p>}
-        {!loading && (
+        {error && <p>{error}</p>}
+        {!error && (
           <ul>
             {items && items.map((product) => (
               <li key={product.id}>{product.name} - R$: {product.price}</li>
--- Seção 7 - Requisições HTTP com React/httpreact/src/hooks/useFetch.js
+++ Seção 7 - Requisições HTTP com React/httpreact/src/hooks/useFetch.js
@@ -12,6 +12,9 @@ export const useFetch = (url) => {
   // 6 - Loading
   const [loading, setLoading] = useState(false);
 
+  // 7 - Tratando erros
+  const [error, setError] = useState(null);
+
   const httpConfig = (data, method) => {
     if (method === 'POST') {
       setConfig({
@@ -32,9 +35,14 @@ export const useFetch = (url) => {
       // 6 - Loading
       setLoading(true);
 
-      const res = await fetch(url);
-      const json = await res.json();
-      setData(json);
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        console.log(err.message);
+        setError('Houve algum erro ao carregar os dados!');
+      }
 
       setLoading(false);
     };
@@ -58,6 +66,6 @@ export const useFetch = (url) => {
     httpRequest();
   }, [config, method, url]);
 
-  return { data, httpConfig, loading };
+  return { data, httpConfig, loading, error };
 };
 
