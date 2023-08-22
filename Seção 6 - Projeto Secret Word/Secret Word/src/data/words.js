ff6affab411d97309e88263d4107ce4f1762e177
Eros Netto <erosnetto1002@gmail.com>
2023-08-21 21:46:26 -0300

    Seção 6 retirada

--- Seção 6 - Projeto Secret Word/Secret Word/src/data/words.js
+++
@@ -1,8 +0,0 @@
-export const wordsList = {
-  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React"],
-  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
-};
