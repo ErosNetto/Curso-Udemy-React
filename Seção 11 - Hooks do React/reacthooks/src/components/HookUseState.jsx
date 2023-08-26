c29ec15222f0115ed351d86577a5705725eb86b9
Eros Netto <erosnetto1002@gmail.com>
2023-08-25 23:04:36 -0300

    UseState

--- Seção 11 - Hooks do React/reacthooks/src/components/HookUseState.jsx
+++ Seção 11 - Hooks do React/reacthooks/src/components/HookUseState.jsx
@@ -14,6 +14,16 @@ const HookUseState = () => {
   };
   
   console.log(name);
+
+  // 2 - UseState e input
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // envio a uma API
+    console.log(age);
+  }
   
   return (
     <div>
@@ -22,6 +32,13 @@ const HookUseState = () => {
       <p>Varíavel: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar nomes!</button>
+      {/* 2 - useState e input */}
+      <p>Digite a sua idade:</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+        <input type="submit" value="Enviar" />
+      </form>
+      <p>Você tem {age} anos!</p>
       <hr />
     </div>
   );
